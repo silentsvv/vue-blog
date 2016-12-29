61ab22e009f70fafd1dd796c3865d6bf8abd31eb
silentsvv <silentsvv@outlook.com>
2016-12-29 22:25:23 +0800

    滚动效果

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,10 +1,35 @@
 <template>
-  <div class="header">
+  <div class="header" v-bind:class="{ scroll: isActive }">
     <div class="link">
       <router-link to="/index" class="index">首页</router-link>
     </div>
   </div>
 </template>
+
+<script type="text/javascript">
+  export default {
+    data(){
+      return {
+        isActive: false
+      }
+    },
+    methods:{
+      myScroll(){
+        if(window.scrollY > 350){
+          this.isActive = true;
+          console.log(this.isActive)
+        }
+        if(window.scrollY < 350){
+          this.isActive = false;
+          console.log(this.isActive)
+        }
+      }
+    },
+    created() {
+      window.addEventListener('scroll', this.myScroll);
+    }
+  }
+</script>
 
 <style lang='less'>
   .header{
@@ -12,6 +37,7 @@
     width: 100%;
     height: 80px;
     z-index: 99;
+    transition:all 1s;
   }
   .link{
     position: relative;
@@ -38,5 +64,14 @@
       border-radius: 3px;
     }
   }
-
+   .scroll{
+    color: #666;
+    opacity: 1;
+    background: #fff;
+    transition:all 1s;
+    box-shadow: 0 0 25px 0 #ccc;
+    .index{
+      color: #666;
+    }
+  }
 </style>
